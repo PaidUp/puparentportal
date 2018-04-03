ee532e87e39008a6d79fac0c73e1deadb96883ea
Diego Fortes <daigof@gmail.com>
2018-04-03 12:52:44 -0300

    payment page doc signature and checkboxes

--- src/components/pages/PaymentsPage.vue
+++ src/components/pages/PaymentsPage.vue
@@ -159,12 +159,33 @@
         <md-button class="lblue md-accent">CANCEL</md-button>
       </md-step>
       <md-step id="step6" md-label="Document Signature">
-         <div class="doc-signature md-elevation-2" @click="setDone('step2', 'step3')">13U Redgional</div>
-         
+        <div class="docs-signature">
+          <div class="doc-signature md-elevation-2 title cblue" @click="setDone('step6', 'step7')">Isotopes Baseball Club Code of Conduct Agreement 2019 & Financial Agreement 2019</div>
+          <div class="doc-signature md-elevation-2 title cblue" @click="setDone('step6', 'step7')">Isotopes Baseball Club Code of Conduct Agreement 2019 & Financial Agreement 2019</div>
+        </div>
       </md-step>
       <md-step id="step7" md-label="Review &amp; Approve">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias doloribus eveniet quaerat modi cumque quos sed,
-          temporibus nemo eius amet aliquid, illo minus blanditiis tempore, dolores voluptas dolore placeat nulla.</p>
+        <div class="review-checks">
+          <md-checkbox v-model="check1" class="">
+            I authorize PaiUp to charge me
+            <span class="cgreen">
+              <b>$600</b>
+            </span>
+            <b>today</b> on my Visa****2323
+          </md-checkbox>
+          <md-checkbox v-model="check2" class="">
+            I authorize PaiUp to setup the remaining
+            <span class="cgreen">
+              <b>$900</b>
+            </span>
+            <b>on autopay</b> on the dates and amount in the payment plan I selected
+          </md-checkbox>
+          <md-checkbox v-model="check3" class="">
+            I agree that PaidUp cannot modify or cancel any payments without approval from the club
+          </md-checkbox>
+        </div>
+        <md-button class="lblue md-accent md-raised">AUTHORIZE PAYMENTS</md-button>
+        <md-button class="lblue md-accent">CANCEL</md-button>
       </md-step>
     </md-steppers>
   </div>
@@ -182,6 +203,9 @@
         step1: false,
         step2: false,
         step3: false,
+        check1: false,
+        check2: false,
+        check3: false,
         selectedDate: null
       }
     },
