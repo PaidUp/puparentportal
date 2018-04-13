42766c5d67e7f12bc5c1d1427838fcff28a60141
Ricardo Lara <riclara@gmail.com>
2018-04-13 14:05:58 -0500

    partial commit payment

--- src/components/pages/paymentPage/VPlayers.vue
+++ src/components/pages/paymentPage/VPlayers.vue
@@ -1,16 +1,16 @@
 <template lang="pug">
   md-step(id="step1" md-label="Player" md-description="Choose a player or add a new one" :md-done.sync="step")
     .players
-      .player.md-elevation-2(@click="select('@@@Param')")
+      .player.md-elevation-2(@click="select(beneficiary)" v-for="beneficiary in beneficiaries" :key="beneficiary._id")
         md-avatar.md-elevation-4
           img(src="@/assets/avatar.jpg")
-        .name.md-body-2 Enzo Fernandez
-        .team.md-caption Isotopes Baseball Club
+        .name.md-body-2 {{ beneficiary.firstName }} {{ beneficiary.firstLastName }}
+        .team.md-caption {{ beneficiary.organizationName }}
     md-button.lblue.md-accent.md-raised ADD NEW PLAYER
     md-button.lblue.md-accent CANCEL
 </template>
 <script>
-// import { mapState, mapMutations } from 'vuex'
+import { mapState } from 'vuex'
 
 export default {
   props: {
@@ -18,18 +18,15 @@ export default {
   },
   data () {
     return {
-      player: null
     }
   },
   computed: {
-    // ...mapState('paymentModule', {
-    //   step1: 'step1'
-    // })
+    ...mapState('playerModule', {
+      orders: 'orders',
+      beneficiaries: 'beneficiaries'
+    })
   },
   methods: {
-    // ...mapMutations('paymentModule', {
-    //   setDone: 'setDone'
-    // })
     select (param) {
       console.log('param: ', param)
       this.$emit('select', param)
