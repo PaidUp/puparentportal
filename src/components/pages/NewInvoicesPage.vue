dd9f942f569a07cf3ec1e16211fdb5e58a92c13f
Diego Fortes <daigof@gmail.com>
2018-05-07 08:56:35 -0300

    New invoice available screen

---
+++ src/components/pages/NewInvoicesPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="common-page new-invoices-page">
+    <div class="title">Make/Schedule Payment</div>
+    <div class="title-info">A new invoice is available for your review and approval, or make/schedule a payment for a standard payment plan.</div>
+    <div class="common-player-info">
+      <md-avatar class="md-large">
+        <img src="@/assets/avatar.jpg" alt="avatar">
+      </md-avatar>
+      <div class="name">
+        Felipe Fernandez
+      </div>
+      <div class="team">
+        Isotopes Club
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        name: 'Diego'
+      }
+    },
+    methods: {}
+  }
+</script>
