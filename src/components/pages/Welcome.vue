08256c6dd9390c596ee6af60e545f027a9ca622e
Diego Fortes <daigof@gmail.com>
2018-05-07 10:06:24 -0300

    welcome page

--- src/components/pages/Welcome.vue
+++ src/components/pages/Welcome.vue
@@ -1,27 +1,31 @@
 <template>
-  <div>
-    <div>{{ $t('component.main.welcome') }}</div>
+  <div class="welcome-page">
+    <div class="title">
+      Welcome to PaidUp Felipe.
+    </div>
+    <div class="title-info">Let's get started by adding a new player</div>
+    <md-button class="md-raised md-accent lblue">ADD NEW PLAYER</md-button>
+    <img src="@/assets/icons/ico08-01.svg" alt="welcome">
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+  import {
+    mapState
+  } from 'vuex'
 
-export default {
-  components: {
-  },
-  data: function () {
-    return {
-      showAddCardDialog: false
+  export default {
+    components: {},
+    data: function () {
+      return {
+        showAddCardDialog: false
+      }
+    },
+    methods: {},
+    computed: {
+      ...mapState('userModule', {
+        user: 'user'
+      })
     }
-  },
-  methods: {
-  },
-  computed: {
-    ...mapState('userModule', {
-      user: 'user'
-    })
   }
-}
 </script>
-
