c08c22aa7a05464d3bc89485d88ee4a982bbf812
Diego Fortes <daigof@gmail.com>
2018-05-16 13:47:36 -0300

    edit player info component add Menu icon

--- src/components/shared/VPlayerInfo.vue
+++ src/components/shared/VPlayerInfo.vue
@@ -1,10 +1,19 @@
 <template lang="pug">
-  .v-player-info
-    md-avatar
-      img(:src="avatarUrl")
-    .name.md-title {{player.firstName}} {{player.lastName}}
-    .team.md-subheading {{ player.organizationName }}
-    md-button.md-primary(@click="editPlayerDialog = true") Edit
+  .common-player-info
+    .with-menu-box
+      div.player-info
+        md-avatar.md-large
+          img(:src="avatarUrl")
+        .name.md-title {{player.firstName}} {{player.lastName}}
+        .team.md-subheading {{ player.organizationName }}
+      md-menu(md-size="small" md-direction="bottom-start")
+        md-button(md-menu-trigger class="md-icon-button md-raised md-accent md-dense lblue")
+          md-icon menu
+        md-menu-content.custom-menu-content
+          md-menu-item MAKE NEW PAYMENT(S)
+          md-menu-item(@click="editPlayerDialog = true") EDIT PLAYER
+    </md-menu>
+      
     EditPlayerDialog(:showDialog="editPlayerDialog")
 </template>
 
