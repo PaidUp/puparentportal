79367b59d41a530b15ba6f835e5703bf32c0df39
Ricardo Lara <riclara@gmail.com>
2018-05-23 15:46:33 -0500

    show credits in checkout flow

---
+++ src/components/pages/paymentPage/VPaymentPlanCreditDetails.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="payment-single md-elevation-2">
+            <div>
+              <div class="title cgray bold">{{ due.description }}</div>
+              <div class="caption"></div>
+            </div>
+            <div class="details">
+              <div class="icon-side">
+                <md-icon class="md-size-c cgreen">today</md-icon>
+                <div class="caption cgreen">{{ due.status.toUpperCase() }}</div>
+              </div>
+              <div class="amount-side">
+                <div class="caption">{{ $d(due.dateCharge, 'short') }}</div>
+                <v-currency :amount="due.amount" :clazz="'cgreen number-big'" />
+              </div>
+            </div>
+            <div class="actions">
+              &nbsp;
+            </div>
+          </div>
+</template>
+<script>
+import VCurrency from '@/components/shared/VCurrency.vue'
+
+export default {
+  components: { VCurrency },
+  props: {
+    due: Object
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
