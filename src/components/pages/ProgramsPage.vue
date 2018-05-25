8cdb128397f35d730de28bf2c04da88bf6ff9c32
Diego Fortes <daigof@gmail.com>
2018-05-25 12:14:55 -0300

    add more cards

--- src/components/pages/ProgramsPage.vue
+++ src/components/pages/ProgramsPage.vue
@@ -98,6 +98,589 @@
           </div>
         </md-card>
 
+        <md-card md-with-hover>
+          <div>
+            <div class="title">12U Red</div>
+            <div class="caption">9 players</div>
+          </div>
+          <div class="main-box">
+            <div class="eligibility-box">
+              <div>
+                <div class="concept">
+                  Eligible
+                </div>
+                <div class="number">7</div>
+              </div>
+              <div>
+                <div class="concept">
+                  Ineligible
+                </div>
+                <div class="number cred bolder">9</div>
+              </div>
+            </div>
+            <div class="total">
+              <div class="tot-title">Total</div>
+              <div class="tot-number">$38,000</div>
+            </div>
+            <div class="bars">
+              <div class="green">
+                <div class="hover">
+                  <div class="hover-title">Paid</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="gray">
+                <div class="hover">
+                  <div class="hover-title">Lack</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="red">
+                <div class="hover">
+                  <div class="hover-title">Overdue</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="blue">
+                <div class="hover">
+                  <div class="hover-title">Some</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </md-card>
+
+        <md-card md-with-hover>
+          <div>
+            <div class="title">12U Red</div>
+            <div class="caption">9 players</div>
+          </div>
+          <div class="main-box">
+            <div class="eligibility-box">
+              <div>
+                <div class="concept">
+                  Eligible
+                </div>
+                <div class="number">7</div>
+              </div>
+              <div>
+                <div class="concept">
+                  Ineligible
+                </div>
+                <div class="number cred bolder">9</div>
+              </div>
+            </div>
+            <div class="total">
+              <div class="tot-title">Total</div>
+              <div class="tot-number">$38,000</div>
+            </div>
+            <div class="bars">
+              <div class="green">
+                <div class="hover">
+                  <div class="hover-title">Paid</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="gray">
+                <div class="hover">
+                  <div class="hover-title">Lack</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="red">
+                <div class="hover">
+                  <div class="hover-title">Overdue</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="blue">
+                <div class="hover">
+                  <div class="hover-title">Some</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </md-card>
+
+        <md-card md-with-hover>
+          <div>
+            <div class="title">12U Red</div>
+            <div class="caption">9 players</div>
+          </div>
+          <div class="main-box">
+            <div class="eligibility-box">
+              <div>
+                <div class="concept">
+                  Eligible
+                </div>
+                <div class="number">7</div>
+              </div>
+              <div>
+                <div class="concept">
+                  Ineligible
+                </div>
+                <div class="number cred bolder">9</div>
+              </div>
+            </div>
+            <div class="total">
+              <div class="tot-title">Total</div>
+              <div class="tot-number">$38,000</div>
+            </div>
+            <div class="bars">
+              <div class="green">
+                <div class="hover">
+                  <div class="hover-title">Paid</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="gray">
+                <div class="hover">
+                  <div class="hover-title">Lack</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="red">
+                <div class="hover">
+                  <div class="hover-title">Overdue</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="blue">
+                <div class="hover">
+                  <div class="hover-title">Some</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </md-card>
+
+        <md-card md-with-hover>
+          <div>
+            <div class="title">12U Red</div>
+            <div class="caption">9 players</div>
+          </div>
+          <div class="main-box">
+            <div class="eligibility-box">
+              <div>
+                <div class="concept">
+                  Eligible
+                </div>
+                <div class="number">7</div>
+              </div>
+              <div>
+                <div class="concept">
+                  Ineligible
+                </div>
+                <div class="number cred bolder">9</div>
+              </div>
+            </div>
+            <div class="total">
+              <div class="tot-title">Total</div>
+              <div class="tot-number">$38,000</div>
+            </div>
+            <div class="bars">
+              <div class="green">
+                <div class="hover">
+                  <div class="hover-title">Paid</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="gray">
+                <div class="hover">
+                  <div class="hover-title">Lack</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="red">
+                <div class="hover">
+                  <div class="hover-title">Overdue</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="blue">
+                <div class="hover">
+                  <div class="hover-title">Some</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </md-card>
+
+        <md-card md-with-hover>
+          <div>
+            <div class="title">12U Red</div>
+            <div class="caption">9 players</div>
+          </div>
+          <div class="main-box">
+            <div class="eligibility-box">
+              <div>
+                <div class="concept">
+                  Eligible
+                </div>
+                <div class="number">7</div>
+              </div>
+              <div>
+                <div class="concept">
+                  Ineligible
+                </div>
+                <div class="number cred bolder">9</div>
+              </div>
+            </div>
+            <div class="total">
+              <div class="tot-title">Total</div>
+              <div class="tot-number">$38,000</div>
+            </div>
+            <div class="bars">
+              <div class="green">
+                <div class="hover">
+                  <div class="hover-title">Paid</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="gray">
+                <div class="hover">
+                  <div class="hover-title">Lack</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="red">
+                <div class="hover">
+                  <div class="hover-title">Overdue</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="blue">
+                <div class="hover">
+                  <div class="hover-title">Some</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </md-card>
+
+        <md-card md-with-hover>
+          <div>
+            <div class="title">12U Red</div>
+            <div class="caption">9 players</div>
+          </div>
+          <div class="main-box">
+            <div class="eligibility-box">
+              <div>
+                <div class="concept">
+                  Eligible
+                </div>
+                <div class="number">7</div>
+              </div>
+              <div>
+                <div class="concept">
+                  Ineligible
+                </div>
+                <div class="number cred bolder">9</div>
+              </div>
+            </div>
+            <div class="total">
+              <div class="tot-title">Total</div>
+              <div class="tot-number">$38,000</div>
+            </div>
+            <div class="bars">
+              <div class="green">
+                <div class="hover">
+                  <div class="hover-title">Paid</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="gray">
+                <div class="hover">
+                  <div class="hover-title">Lack</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="red">
+                <div class="hover">
+                  <div class="hover-title">Overdue</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="blue">
+                <div class="hover">
+                  <div class="hover-title">Some</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </md-card>
+
+        <md-card md-with-hover>
+          <div>
+            <div class="title">12U Red</div>
+            <div class="caption">9 players</div>
+          </div>
+          <div class="main-box">
+            <div class="eligibility-box">
+              <div>
+                <div class="concept">
+                  Eligible
+                </div>
+                <div class="number">7</div>
+              </div>
+              <div>
+                <div class="concept">
+                  Ineligible
+                </div>
+                <div class="number cred bolder">9</div>
+              </div>
+            </div>
+            <div class="total">
+              <div class="tot-title">Total</div>
+              <div class="tot-number">$38,000</div>
+            </div>
+            <div class="bars">
+              <div class="green">
+                <div class="hover">
+                  <div class="hover-title">Paid</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="gray">
+                <div class="hover">
+                  <div class="hover-title">Lack</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="red">
+                <div class="hover">
+                  <div class="hover-title">Overdue</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="blue">
+                <div class="hover">
+                  <div class="hover-title">Some</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </md-card>
+
+        <md-card md-with-hover>
+          <div>
+            <div class="title">12U Red</div>
+            <div class="caption">9 players</div>
+          </div>
+          <div class="main-box">
+            <div class="eligibility-box">
+              <div>
+                <div class="concept">
+                  Eligible
+                </div>
+                <div class="number">7</div>
+              </div>
+              <div>
+                <div class="concept">
+                  Ineligible
+                </div>
+                <div class="number cred bolder">9</div>
+              </div>
+            </div>
+            <div class="total">
+              <div class="tot-title">Total</div>
+              <div class="tot-number">$38,000</div>
+            </div>
+            <div class="bars">
+              <div class="green">
+                <div class="hover">
+                  <div class="hover-title">Paid</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="gray">
+                <div class="hover">
+                  <div class="hover-title">Lack</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="red">
+                <div class="hover">
+                  <div class="hover-title">Overdue</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="blue">
+                <div class="hover">
+                  <div class="hover-title">Some</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </md-card>
+
+        <md-card md-with-hover>
+          <div>
+            <div class="title">12U Red</div>
+            <div class="caption">9 players</div>
+          </div>
+          <div class="main-box">
+            <div class="eligibility-box">
+              <div>
+                <div class="concept">
+                  Eligible
+                </div>
+                <div class="number">7</div>
+              </div>
+              <div>
+                <div class="concept">
+                  Ineligible
+                </div>
+                <div class="number cred bolder">9</div>
+              </div>
+            </div>
+            <div class="total">
+              <div class="tot-title">Total</div>
+              <div class="tot-number">$38,000</div>
+            </div>
+            <div class="bars">
+              <div class="green">
+                <div class="hover">
+                  <div class="hover-title">Paid</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="gray">
+                <div class="hover">
+                  <div class="hover-title">Lack</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="red">
+                <div class="hover">
+                  <div class="hover-title">Overdue</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="blue">
+                <div class="hover">
+                  <div class="hover-title">Some</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </md-card>
+
+        <md-card md-with-hover>
+          <div>
+            <div class="title">12U Red</div>
+            <div class="caption">9 players</div>
+          </div>
+          <div class="main-box">
+            <div class="eligibility-box">
+              <div>
+                <div class="concept">
+                  Eligible
+                </div>
+                <div class="number">7</div>
+              </div>
+              <div>
+                <div class="concept">
+                  Ineligible
+                </div>
+                <div class="number cred bolder">9</div>
+              </div>
+            </div>
+            <div class="total">
+              <div class="tot-title">Total</div>
+              <div class="tot-number">$38,000</div>
+            </div>
+            <div class="bars">
+              <div class="green">
+                <div class="hover">
+                  <div class="hover-title">Paid</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="gray">
+                <div class="hover">
+                  <div class="hover-title">Lack</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="red">
+                <div class="hover">
+                  <div class="hover-title">Overdue</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="blue">
+                <div class="hover">
+                  <div class="hover-title">Some</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </md-card>
+
+        <md-card md-with-hover>
+          <div>
+            <div class="title">12U Red</div>
+            <div class="caption">9 players</div>
+          </div>
+          <div class="main-box">
+            <div class="eligibility-box">
+              <div>
+                <div class="concept">
+                  Eligible
+                </div>
+                <div class="number">7</div>
+              </div>
+              <div>
+                <div class="concept">
+                  Ineligible
+                </div>
+                <div class="number cred bolder">9</div>
+              </div>
+            </div>
+            <div class="total">
+              <div class="tot-title">Total</div>
+              <div class="tot-number">$38,000</div>
+            </div>
+            <div class="bars">
+              <div class="green">
+                <div class="hover">
+                  <div class="hover-title">Paid</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="gray">
+                <div class="hover">
+                  <div class="hover-title">Lack</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="red">
+                <div class="hover">
+                  <div class="hover-title">Overdue</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+              <div class="blue">
+                <div class="hover">
+                  <div class="hover-title">Some</div>
+                  <div class="hover-number">$38,000</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </md-card>
+
 
       </div>
     </div>
