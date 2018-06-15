3ac7d8f15151b95ad694eeabbc5f12cc63f64718
Jeny Alejandra Mazo <yenymzo@gmail.com>
2018-06-15 07:12:50 -0500

    changing invoice date text

--- src/components/shared/ViewInvoiceDialog.vue
+++ src/components/shared/ViewInvoiceDialog.vue
@@ -19,7 +19,7 @@
             <div>
             </div>
             <div>
-              Invoice Approval Date
+              Invoice Approval Date: {{ $d(date, 'short') }}
             </div>
           </div>
           <div class="instructions">
