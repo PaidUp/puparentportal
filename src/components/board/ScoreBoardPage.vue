472dc8ee431b7f57449a19f7239ddf4a057e1abd
Ricardo Lara <riclara@gmail.com>
2018-06-25 15:07:02 -0500

    move reduce to scoreboard parent component

--- src/components/board/ScoreBoardPage.vue
+++ src/components/board/ScoreBoardPage.vue
@@ -4,16 +4,17 @@
       <div class="pre-cards-title">Details</div>
       <div class="details-box">
         <pu-details-selects :programId="programId" @selectSeason="setSeason" @selectProgram="setProgram" :items="items"></pu-details-selects>
-        <pu-details-totals :items="items"></pu-details-totals>
+        <pu-details-totals :items="itemsFiltered"></pu-details-totals>
       </div>
     </div>
-    <pu-products v-show="!programSelected" @programSelected="selectProgram" @setItems="setItems" :seasonId="seasonId" :programId="programId"></pu-products>
+    <pu-products v-if="!programSelected" @programSelected="selectProgram" :items="itemsFiltered"></pu-products>
     <pu-players v-if="programSelected" :seasonId="seasonId" :productId="programId"></pu-players>
   </div>
 
 </template>
 
 <script>
+  import { mapState, mapActions } from 'vuex'
   import PuDetailsSelects from './score_board/PUDetailsSelects.vue'
   import PuDetailsTotals from './score_board/PUDetailsTotals.vue'
   import PuProducts from './score_board/PUProducts.vue'
@@ -29,7 +30,38 @@
         programId: null
       }
     },
+    computed: {
+      ...mapState('userModule', {
+        'user': 'user'
+      }),
+      itemsFiltered () {
+        if (this.programId) {
+          let resp = {}
+          resp[this.programId] = this.items[this.programId]
+          return resp
+        }
+        return this.items
+      }
+    },
+    watch: {
+      seasonId () {
+        this.getAll()
+      },
+      programId () {
+        this.getAll()
+      }
+    },
     methods: {
+      ...mapActions('organizationModule', {
+        getReducePrograms: 'getReducePrograms'
+      }),
+      getAll () {
+        if (this.user && this.seasonId) {
+          this.getReducePrograms({organizationId: this.user.organizationId, seasonId: this.seasonId}).then(items => {
+            this.items = items
+          })
+        }
+      },
       setSeason (season) {
         this.programId = null
         this.seasonId = season
--- src/components/board/score_board/PUProducts.vue
+++ src/components/board/score_board/PUProducts.vue
@@ -3,61 +3,26 @@
       <div class="pre-cards-title">Programs</div>
       <div class="cards-layout">
 
-        <pu-score-card @programSelected="selectProgram" v-for="item in itemsFiltered" :key="item.id" :item="item"></pu-score-card>
+        <pu-score-card @programSelected="selectProgram" v-for="item in items" :key="item.id" :item="item"></pu-score-card>
 
       </div>  
     </div>
 </template>
 <script>
-  import { mapState, mapActions } from 'vuex'
   import PuScoreCard from './PUScoreCard.vue'
 
   export default {
     components: { PuScoreCard },
     props: {
-      seasonId: String,
-      programId: String
+      items: Object
     },
     data: function () {
       return {
-        items: null
-      }
-    },
-    computed: {
-      ...mapState('userModule', {
-        'user': 'user'
-      }),
-      itemsFiltered () {
-        if (this.programId) {
-          let resp = {}
-          resp[this.programId] = this.items[this.programId]
-          return resp
-        }
-        return this.items
       }
     },
     methods: {
-      ...mapActions('organizationModule', {
-        getReducePrograms: 'getReducePrograms'
-      }),
-      getAll () {
-        if (this.user && this.seasonId) {
-          this.getReducePrograms({organizationId: this.user.organizationId, seasonId: this.seasonId}).then(items => {
-            this.items = items
-            this.$emit('setItems', this.itemsFiltered)
-          })
-        }
-      },
       selectProgram (program) {
         this.$emit('programSelected', program)
-      }
-    },
-    watch: {
-      seasonId () {
-        this.getAll()
-      },
-      programId () {
-        this.getAll()
       }
     }
   }
