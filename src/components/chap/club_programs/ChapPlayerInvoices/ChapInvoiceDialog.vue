cdc7c480d81c3bb8f9e0ffd16285fc00f45491fe
Ricardo Lara <riclara@gmail.com>
2018-07-27 10:06:33 -0500

    add validation to edit or clone invoice

--- src/components/chap/club_programs/ChapPlayerInvoices/ChapInvoiceDialog.vue
+++ src/components/chap/club_programs/ChapPlayerInvoices/ChapInvoiceDialog.vue
@@ -1,8 +1,8 @@
 <template>
   <md-dialog :md-active.sync="showDialog" class="invoice-dialog" v-if="invoice">
-    <div class="dialog-header" v-if="!isClone">
-      <div class="title">Invoice: {{ invoice.invoiceId }}</div>
-      <md-menu md-size="small" md-direction="bottom-end">
+    <div class="dialog-header">
+      <div class="title" v-if="!isClone">Invoice: {{ invoice.seq }}</div>
+      <md-menu v-if="!isClone" md-size="small" md-direction="bottom-end">
         <md-button class="md-icon-button" md-menu-trigger>
           <md-icon>file_download</md-icon>
         </md-button>
@@ -19,54 +19,62 @@
             <div>
             </div>
           </div>
-        <md-field>
+        <md-field :class="{'md-invalid': $v.updInvoice.label.$error}">
           <label>Description</label>
-          <md-input v-model="updInvoice.label"></md-input>
+          <md-input v-model="updInvoice.label" @input="$v.updInvoice.label.$touch()"></md-input>
+          <span class="md-error" v-if="!$v.updInvoice.label.required">{{ $t('validations.required', { field: 'Description' }) }}</span>
         </md-field>
-        <md-field>
+        <md-field :class="{'md-invalid': $v.updInvoice.price.$error}">
           <label>Amount</label>
           <span class="md-prefix">$</span>
-          <md-input v-model="updInvoice.price" :disabled="disabled"></md-input>
+          <md-input v-model="updInvoice.price" :disabled="disabled" @input="$v.updInvoice.price.$touch()"></md-input>
+          <span class="md-error" v-if="!$v.updInvoice.price.required">{{ $t('validations.required', { field: 'Amount' }) }}</span>
+          <span class="md-error" v-if="!$v.updInvoice.price.numeric">{{ $t('validations.numeric', { field: 'Amount' }) }} </span>
         </md-field>
         <md-field v-if="disabled">
-          <label>Charge date</label>
+          <label>Charge Date</label>
           <md-input :value="$d(updInvoice.dateCharge, 'short')" :disabled="disabled"></md-input>
         </md-field>
-        <div v-else>
-          <label>Charge date</label>
-          <md-datepicker class="datepicker-field" v-model="updInvoice.dateCharge"></md-datepicker>
+        <div v-else :class="{'md-invalid': $v.updInvoice.dateCharge.$error}">
+          <label>Charge Date</label>
+          <md-datepicker class="datepicker-field" @input="$v.updInvoice.dateCharge.$touch()" v-model="updInvoice.dateCharge"></md-datepicker>
+          <span class="md-error" v-if="!$v.updInvoice.dateCharge.required">{{ $t('validations.required', { field: 'Charge Date' }) }}</span>
         </div>
         <md-field v-if="disabled">
           <label>Max Charge Date</label>
           <md-input :value="$d(updInvoice.maxDateCharge, 'short')" :disabled="disabled"></md-input>
         </md-field>
-        <div v-else>
+        <div v-else :class="{'md-invalid': $v.updInvoice.maxDateCharge.$error}">
           <label>Max Charge Date</label>
-          <md-datepicker class="datepicker-field" v-model="updInvoice.maxDateCharge"></md-datepicker>
+          <md-datepicker class="datepicker-field" @input="$v.updInvoice.maxDateCharge.$touch()" v-model="updInvoice.maxDateCharge"></md-datepicker>
+          <span class="md-error" v-if="!$v.updInvoice.maxDateCharge.required">{{ $t('validations.required', { field: 'Max Charge Date' }) }}</span>
         </div>
-        <md-field>
+        <md-field :class="{'md-invalid': $v.updInvoice.status.$error}">
           <label>Status</label>
           <md-select disabled v-if="invoice.status !== 'failed'" class="custom-select" v-model="updInvoice.status">
             <md-option value="paidup">Paidup</md-option>
             <md-option value="autopay">Autopay</md-option>
             <md-option value="refunded">Refunded</md-option>
           </md-select>
-          <md-select v-else class="custom-select" v-model="updInvoice.status">
+          <md-select v-else @input="$v.updInvoice.status.$touch()" class="custom-select" v-model="updInvoice.status">
             <md-option value="failed">Failed</md-option>
             <md-option value="autopay">Autopay</md-option>
           </md-select>
+          <span class="md-error" v-if="!$v.updInvoice.status.required">{{ $t('validations.required', { field: 'Status' }) }}</span>
         </md-field>
-        <md-field>
+        <md-field :class="{'md-invalid': $v.parent.$error}">
           <label for="payment">Parent</label>
-          <md-select class="custom-select" v-model="parent" :disabled="disabled">
+          <md-select class="custom-select" v-model="parent" @input="$v.parent.$touch()" :disabled="disabled">
             <md-option v-for="parent in parents" :key="parent._id" :value="parent._id">{{ parent.firstName + ' ' + parent.lastName }}</md-option>
           </md-select>
+          <span class="md-error" v-if="!$v.parent.required">{{ $t('validations.required', { field: 'Parent' }) }}</span>
         </md-field>
-        <md-field>
+        <md-field :class="{'md-invalid': $v.pmSelected.$error}">
           <label for="payment">Payment Account</label>
-          <md-select class="custom-select" v-model="pmSelected" :disabled="disabled">
+          <md-select class="custom-select" v-model="pmSelected" @input="$v.pmSelected.$touch()" :disabled="disabled">
             <md-option v-for="pm in parentPaymentMethods" :key="pm.id" :value="pm.id">{{ pm.brand || pm.bank_name }}••••{{ pm.last4 }}</md-option>
           </md-select>
+          <span class="md-error" v-if="!$v.pmSelected.required">{{ $t('validations.required', { field: 'Payment Account' }) }}</span>
         </md-field>
         
         </div>
@@ -115,8 +123,8 @@
     </md-tabs>
     <md-dialog-actions>
       <md-button class="md-accent lblue" @click="showDialog = false">CANCEL</md-button>
-      <md-button v-if='isClone' :disabled="submited" class="md-accent lblue" @click="clone" >CLONE</md-button>
-      <md-button v-else :disabled="submited" class="md-accent lblue" @click="save" >SAVE</md-button>
+      <md-button v-if='isClone' :disabled="disableSaveButton" class="md-accent lblue" @click="clone" >CLONE</md-button>
+      <md-button v-else :disabled="disableSaveButton" class="md-accent lblue" @click="save" >SAVE</md-button>
     </md-dialog-actions>
     <v-pay-animation :animate="submited"  @finish="showDialog = false" />
   </md-dialog>
@@ -124,6 +132,7 @@
 
 <script>
   import VPayAnimation from '@/components/shared/VPayAnimation.vue'
+  import { required, numeric } from 'vuelidate/lib/validators'
   import { mapState, mapActions } from 'vuex'
 
   export default {
@@ -260,9 +269,6 @@
       }
     },
     computed: {
-      disabled () {
-        return !this.isClone && this.invoice.status !== 'autopay' && this.invoice.status !== 'failed'
-      },
       ...mapState('playerInvoicesModule', {
         beneficiary: 'beneficiary',
         paymentMethods: 'paymentMethods',
@@ -273,6 +279,12 @@
         seasonSelected: 'seasonSelected',
         organization: 'organization'
       }),
+      disabled () {
+        return !this.isClone && this.invoice.status !== 'autopay' && this.invoice.status !== 'failed'
+      },
+      disableSaveButton () {
+        return this.$v.$invalid || this.submmited
+      },
       parentPaymentMethods () {
         if (!this.invoice.user) return []
         if (this.parent) return this.paymentMethods[this.parent]
@@ -297,6 +309,31 @@
           })
         }
         return resp
+      }
+    },
+    validations: {
+      updInvoice: {
+        label: {
+          required
+        },
+        price: {
+          required, numeric
+        },
+        dateCharge: {
+          required
+        },
+        maxDateCharge: {
+          required
+        },
+        status: {
+          required
+        }
+      },
+      pmSelected: {
+        required
+      },
+      parent: {
+        required
       }
     }
   }
