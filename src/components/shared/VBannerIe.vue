8a34c202c561c1ada424a0957cc444f17b6b8d3d
Ricardo Lara <riclara@gmail.com>
2018-07-27 21:01:04 -0500

    add banner ie

---
+++ src/components/shared/VBannerIe.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="banner" v-if="isIE"> 
+    <md-icon style="color: red">warning</md-icon> BROWSER NOT SUPPORTED <br/>
+    PaidUp is currently not supported in your browser. <br/>
+    Please try visiting PaidUp using the latest version of Chrome, Safari or Firefox. <br/>
+  </div>
+</template>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters('uiModule', {
+      isIE: 'isIE'
+    })
+  }
+}
+</script>
+<style>
+  .banner{
+    text-align: center;
+    vertical-align: middle;
+    height: auto;
+    padding: 10px 10px;
+    width: 100%;
+    background: #FFF2D1;
+    color: #83621F;
+    font-size: 18px;
+    font-weight: bold
+  }
+</style>
