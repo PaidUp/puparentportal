e316da6fcb151a144cbdcbb1ee96a22204040e2f
Ricardo Lara <riclara@gmail.com>
2018-11-27 20:56:40 -0500

    remove existURL

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,3 @@
-import { existURL } from './trae-service'
 export { paymentService } from './payment-service'
 export { beneficiaryService } from './organization/beneficiary-service'
 export { organizationService } from './organization/organization-service'
@@ -7,7 +6,3 @@ export { productService } from './organization/product-service'
 export { commerceService } from './commerce-service'
 export { reduceDataCardService } from './reduce-data-card-service'
 export { userService } from './user-service'
-
-export const utilService = {
-  existURL
-}
