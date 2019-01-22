d7c03bd0cf981d2486ad990a44c4021dddb21d6e
Ricardo Lara <riclara@gmail.com>
2019-01-22 14:15:33 -0500

    fix date components

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -8,10 +8,16 @@ export const formatDate = (value) => {
   return moment(value).format(formatStr)
 }
 
+export const localFormatDate = (value) => {
+  if (!value) return value
+  const ltz = moment.tz.guess()
+  return moment.tz(value, ltz).format(formatStr)
+}
+
 export const removeTimeZone = (value) => {
   if (!value) return value
   const ltz = moment.tz.guess()
-  const str = moment.tz(value, ltz).format().substring(0, 10)
-  const res = moment.utc(str).add(16, 'hours')
+  const str = moment.tz(value, ltz).format('YYYY-MM-DD')
+  const res = moment.utc(str.substring(0, 10)).add(16, 'hours')
   return res
 }
